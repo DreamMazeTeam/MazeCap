a5b7385f09a86097e8c68e841e93e0c8cdd127e4
DreamMazeTeam <dmitriiezhow@gmail.com>
2019-12-04 22:56:51 +0300

    Module Loader

--- raspberry/__main__.py
+++ raspberry/__main__.py
@@ -1,1 +1,26 @@
-import modules
+import os
+import json
+
+"""
+
+Кастомный модульный импортер,
+Для того что бы запустить новый модуль,
+Достаточно будет поместить его в папку modules
+
+"""
+
+MODULES = []
+MODULES_PATH = "raspberry.modules"
+
+mod = None
+for module in os.listdir("modules\\"):
+    if "py" in module.split('.'):
+        exec(f"import {MODULES_PATH}.{'.'.join(module.split('.')[:-1])} as mod")
+        MODULES += [mod]
+
+
+if __name__ == '__main__':
+    pass
+
+
+
