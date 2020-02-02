ffd0a52a7ef8db05770776c4c0c3a265b50cee93
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-02-02 15:37:09 +0300

    SSSS

---
+++ arduino/src/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using CBYTE const char;
+
+CBYTE MOVE_FORWARD = 'w';
+CBYTE MOVE_LEFT = 'a';
+CBYTE MOVE_RIGHT = 'd';
+CBYTE MOVE_REVERSE = 's';
+CBYTE LIGHT_BLINCK = 'l';
+CBYTE THOUGH_AID_KIT = 'k';
+
